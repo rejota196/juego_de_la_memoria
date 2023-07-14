913f24ebc1125603ce135458c3aa4c95b2cec1f9
rejota196 <raulej196@gmail.com>
2023-07-14 12:34:58 -0300

    creating button 3d

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private Animator anim;
+    
+
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isRotating) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject) 
+                {
+                    anim.SetTrigger("ToShow");
+                    
+                    
+                }
+            }
+        }
+    }
+
+
+}
